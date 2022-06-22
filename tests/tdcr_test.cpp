cb40cd9aa49ed2b97fd0925dc65ec45a0debc327
Chien Jer Luen <jerluen0@gmail.com>
2022-06-22 17:48:44 +0800

    added LMPC part

--- tests/tdcr_test.cpp
+++ tests/tdcr_test.cpp
@@ -23,17 +23,17 @@ int main() {
     multistage_straight_step_integrator1_acados_sim_create(capsule1step);
     sim_solver_capsule *capsule2step = multistage_straight_integrator2_acados_sim_solver_create_capsule();
     multistage_straight_step_integrator2_acados_sim_create(capsule2step);
-    // nlp_solver_capsule *nlpcapsule = tdcr_lmpc_acados_create_capsule();
-    // tdcr_lmpc_acados_create(nlpcapsule);
+    nlp_solver_capsule *nlpcapsule = tdcr_lmpc_acados_create_capsule();
+    tdcr_lmpc_acados_create(nlpcapsule);
 
-    // std::vector<IntegrationInterface> i;
-    // std::vector<IntegrationInterface> is;
+    std::vector<IntegrationInterface> i;
+    std::vector<IntegrationInterface> is;
 
-    // IntegrationInterface i1(capsule1), i2(capsule2), ii1(capsule1step), ii2(capsule2step); 
-    // i.push_back(i1);
-    // i.push_back(i2);
-    // is.push_back(ii1);
-    // is.push_back(ii2);
+    IntegrationInterface i1(capsule1), i2(capsule2), ii1(capsule1step), ii2(capsule2step); 
+    i.push_back(i1);
+    i.push_back(i2);
+    is.push_back(ii1);
+    is.push_back(ii2);
 
     Eigen::MatrixXd stage_tendons;
     stage_tendons.resize(2, 6);
@@ -57,52 +57,52 @@ int main() {
 
     } 
 
-    // MultistageTDCR_Solver tendon_robot(6, 2, i, is, stage_tendons, routing);
-    // ControllerInterface controller(nlpcapsule);
-    // TDCR_Interface c(&tendon_robot, &controller); 
+    MultistageTDCR_Solver tendon_robot(6, 2, i, is, stage_tendons, routing);
+    ControllerInterface controller(nlpcapsule);
+    TDCR_Interface c(&tendon_robot, &controller); 
 
-    // Eigen::MatrixXd tau(6, 1); 
-    // tau << 0.0, 0.0, 0.0, 0.0, 0.0, 0.0; 
-    // c.solveForwardKinematics(tau, false);
+    Eigen::MatrixXd tau(6, 1); 
+    tau << 0.0, 0.0, 0.0, 0.0, 0.0, 0.0; 
+    c.solveForwardKinematics(tau, false);
 
-    // Eigen::MatrixXi w1(2, 1), w2(6, 1);
-    // Eigen::MatrixXd desiredPose(8, 1), weight_all(8, 8), controlInput; 
-    // std::vector<Eigen::MatrixXi> CSM; 
-    // Eigen::MatrixXi CS(2, 1); 
-    // CS << 0, 1;
-    // w1 << 0, 1; 
-    // w2 << 0, 1, 3, 4, 5, 6; 
-    // CSM.push_back(w1); 
-    // CSM.push_back(w2); 
-    // c.setDimensions(8, CSM, CS);
+    Eigen::MatrixXi w1(2, 1), w2(6, 1);
+    Eigen::MatrixXd desiredPose(8, 1), weight_all(8, 8), controlInput; 
+    std::vector<Eigen::MatrixXi> CSM; 
+    Eigen::MatrixXi CS(2, 1); 
+    CS << 0, 1;
+    w1 << 0, 1; 
+    w2 << 0, 1, 3, 4, 5, 6; 
+    CSM.push_back(w1); 
+    CSM.push_back(w2); 
+    c.setDimensions(8, CSM, CS);
 
-    // desiredPose << 0.0, 0.04, -0.2, 0.2, 1., 0., 0., 0.;
+    desiredPose << 0.0, 0.04, -0.2, 0.2, 1., 0., 0., 0.;
 
-    // c.setWeightsAllStages(weight_all);
-    // c.timer.tic();
+    c.setWeightsAllStages(weight_all);
+    c.timer.tic();
 
-    // for (int i = 0; i < 500; ++i) 
+    for (int i = 0; i < 500; ++i) 
     
-    // {
+    {
 
-    //     controlInput = c.getHighLevelControl(desiredPose);
+        controlInput = c.getHighLevelControl(desiredPose);
         
-    //     c.simulateStep(controlInput);
-    //     // std::cout << "Checking Boundary Conditions: " << c.checkBoundaryConditions() << "\n"; 
+        c.simulateStep(controlInput);
+        // std::cout << "Checking Boundary Conditions: " << c.checkBoundaryConditions() << "\n"; 
        
 
-    // }
+    }
 
-    // c.timer.toc();
+    c.timer.toc();
 
-    // // std::cout << "Error: \n" << c.getCustomPoseError() << "\n\n";
-    // std::cout << "customPose: " << c.getCustomPose().transpose() << std::endl;
-    // std::cout << "Required Tau: \n" << c.getTau().transpose() << std::endl;
+    // std::cout << "Error: \n" << c.getCustomPoseError() << "\n\n";
+    std::cout << "customPose: " << c.getCustomPose().transpose() << std::endl;
+    std::cout << "Required Tau: \n" << c.getTau().transpose() << std::endl;
 
-    // free(capsule1);
-    // free(capsule1step);
-    // free(capsule2);
-    // free(capsule2step);
+    free(capsule1);
+    free(capsule1step);
+    free(capsule2);
+    free(capsule2step);
 
     return 0; 
 
